4b4ef79560ac6b2cb377dc36f5c887a5255037e8
Shourya Naik <sjnaik02@gmail.com>
2023-03-23 13:45:31 +0530

    minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -9,12 +9,13 @@ const oswald = Oswald({
 
 const manrope = Manrope({
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-manrope",
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${oswald.variable} font-sans ${manrope.variable} `}>
+    <main className={`${oswald.variable} font-sans ${manrope.variable}`}>
       <Component {...pageProps} />
     </main>
   );
