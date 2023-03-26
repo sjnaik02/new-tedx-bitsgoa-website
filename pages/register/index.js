73ab475efcf488ab05a9867207ee0fa6b0d96d39
Shourya Naik <sjnaik02@gmail.com>
2023-03-26 17:26:53 +0530

    added a loading indicator

--- pages/register/index.js
+++ pages/register/index.js
@@ -5,10 +5,12 @@ export default function index() {
   const [id, setId] = useState("");
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const scriptUrl =
     "https://script.google.com/macros/s/AKfycbyXBVbSKoRQ6FdKILcMsO3_DJwa6X55SYSNVtjJd54rbcKvruPp0PbRVA3rIkWcL-Id1A/exec";
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const res = await fetch(scriptUrl, {
       method: "POST",
       body: new FormData(e.target),
@@ -22,6 +24,7 @@ export default function index() {
     }
     setName("");
     setId("");
+    setLoading(false);
   };
   return (
     <div className="flex h-[100svh] w-full bg-black font-body text-white md:flex-row">
@@ -46,58 +49,62 @@ export default function index() {
           {success === true && (
             <p className="text-lg text-white">Sucessfully Registered!</p>
           )}
-          <form
-            className="mt-4 flex flex-col rounded-lg border border-gray-700 p-4"
-            method="post"
-            action="https://script.google.com/macros/s/AKfycbyXBVbSKoRQ6FdKILcMsO3_DJwa6X55SYSNVtjJd54rbcKvruPp0PbRVA3rIkWcL-Id1A/exec"
-            onSubmit={handleSubmit}
-          >
-            <label htmlFor="Name" className="text-lg" hidden>
-              Name:
-            </label>
-            <input
-              type="text"
-              name="Name"
-              id="name"
-              className="mt-2 rounded-md border border-gray-700 bg-black px-4 py-2 text-lg focus:border-red-600 focus:outline-none"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Name"
-              required
-            />
-            <label htmlFor="BITS ID" className="text-lg" hidden>
-              BITS ID:
-            </label>
-            <input
-              type="text"
-              name="BITS ID"
-              id="id"
-              className="mt-2 rounded-md border border-gray-700 bg-black px-4 py-2 text-lg focus:border-red-600 focus:outline-none"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              placeholder="BITS ID"
-              required
-            />
-            <div className="mt-4 flex">
+          {!loading ? (
+            <form
+              className="mt-4 flex flex-col rounded-lg border border-gray-700 p-4"
+              method="post"
+              action="https://script.google.com/macros/s/AKfycbyXBVbSKoRQ6FdKILcMsO3_DJwa6X55SYSNVtjJd54rbcKvruPp0PbRVA3rIkWcL-Id1A/exec"
+              onSubmit={handleSubmit}
+            >
+              <label htmlFor="Name" className="text-lg" hidden>
+                Name:
+              </label>
               <input
-                type="checkbox"
-                name="SWD"
-                id="SWD"
-                className="mt-2 checked:accent-red-600"
+                type="text"
+                name="Name"
+                id="name"
+                className="mt-2 rounded-md border border-gray-700 bg-black px-4 py-2 text-lg focus:border-red-600 focus:outline-none"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
                 required
               />
-              <label htmlFor="SWD" className="ml-4 text-base">
-                I give my consent to deduct Rs. 650/- from my SWD account to
-                TEDxBITSGoa to register for TEDxBITSGoa 2023.
+              <label htmlFor="BITS ID" className="text-lg" hidden>
+                BITS ID:
               </label>
-            </div>
-            <button
-              type="submit"
-              className="mt-4 rounded-md bg-red-600 px-4 py-2 text-lg font-bold uppercase text-white hover:bg-red-700 focus:bg-red-700 focus:outline-none"
-            >
-              Register
-            </button>
-          </form>
+              <input
+                type="text"
+                name="BITS ID"
+                id="id"
+                className="mt-2 rounded-md border border-gray-700 bg-black px-4 py-2 text-lg focus:border-red-600 focus:outline-none"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                placeholder="BITS ID"
+                required
+              />
+              <div className="mt-4 flex">
+                <input
+                  type="checkbox"
+                  name="SWD"
+                  id="SWD"
+                  className="mt-2 checked:accent-red-600"
+                  required
+                />
+                <label htmlFor="SWD" className="ml-4 text-base">
+                  I give my consent to deduct Rs. 650/- from my SWD account to
+                  TEDxBITSGoa to register for TEDxBITSGoa 2023.
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="mt-4 rounded-md bg-red-600 px-4 py-2 text-lg font-bold uppercase text-white hover:bg-red-700 focus:bg-red-700 focus:outline-none"
+              >
+                Register
+              </button>
+            </form>
+          ) : (
+            <p className="animate-pulse text-lg text-white">Loading...</p>
+          )}
         </div>
       </div>
       <div className="absolute top-0 h-24 w-full bg-gradient-to-br from-red-900 via-black to-red-600 sm:static sm:h-full sm:w-1/2" />
