295a1661e7eaca9f31e99c665668fa305c712790
Shourya Naik <sjnaik02@gmail.com>
2023-04-02 14:11:12 +0530

    changed favicon, removed excess assets

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      {/* make /assets/cool_x the favicon */}
+      <link rel="icon" href="/assets/cool_x.png" />
+      <link rel="mask-icon" href="/assets/cool_x.png" color="#000000" />
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
