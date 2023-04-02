40681da559bf45ecd317219741a0042692b36996
Shourya Naik <sjnaik02@gmail.com>
2023-04-02 20:12:00 +0530

    some changes to the sponsor card

--- components/SponsorCard.jsx
+++ components/SponsorCard.jsx
@@ -1,14 +1,20 @@
 export default function SponsorCard({ imageUrl, name, title, large }) {
   return (
     <div
-      className={`group flex h-full  w-full flex-col items-center gap-4 rounded-lg  p-4 ${
-        large ? "col-span-2 col-start-1 row-start-1 lg:col-start-2" : ""
+      className={`group flex h-full  w-full flex-col items-center gap-4 rounded-lg p-4 ${
+        large ? "col-span-full col-start-1 row-start-1" : ""
+      }
+      ${name === "Beardo" ? "lg:col-span-1 lg:col-start-2" : ""}
+      ${
+        name === "EaseMyTrip" ? "md:scale-125 lg:col-span-1 lg:col-start-3" : ""
       }`}
     >
       <img
         src={imageUrl}
         alt={name}
-        className="h-48 w-auto object-contain grayscale transition-all duration-500 group-hover:grayscale-0"
+        className={`${
+          title === "Past Sponsor" ? "h-36" : "h-48"
+        } w-auto object-contain  transition-all `}
       />
       <div className="hidden">
         <h3 className="font-heading text-2xl ">{title}</h3>
