19c53a6770789e6235cfc3d07c7bf0dcf99a7f02
Shourya Naik <sjnaik02@gmail.com>
2023-04-02 22:57:47 +0530

    Search engine optimising, adding og tags and images

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       {/* make /assets/cool_x the favicon */}
-      <link rel="icon" href="/assets/cool_x.png" />
-      <link rel="mask-icon" href="/assets/cool_x.png" color="#000000" />
       <Head />
       <body>
         <Main />
