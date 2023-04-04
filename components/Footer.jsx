ca0077941e206a8056a84e9f9af4d87ef0bedb93
Shourya Naik <sjnaik02@gmail.com>
2023-04-04 15:58:08 +0530

    added contact section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,11 +2,52 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="font-bod flex h-36 w-full flex-col items-center justify-center text-center	text-lg text-white md:text-xl ">
-      <p className="font-heading">
+    <footer className="min-h-36 flex w-full flex-col items-center justify-center pb-4 text-center font-body	text-lg text-white md:text-xl ">
+      <div className="mb-4 text-left">
+        <p className="font-heading text-xl md:text-2xl">Contact Us:</p>
+        {/* mail to tedxbitsgoa2023 and phone number  */}
+        <a
+          href="mailto:tedxbitsgoa2023@gmail.com"
+          className="text-xl text-red-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="mr-2 inline h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+            />
+          </svg>
+          tedxbitsgoa2023@gmail.com
+        </a>
+        <p className="text-xl text-red-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="mr-1 inline h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+            />
+          </svg>
+          +91 6291-877574
+        </p>
+      </div>
+      <p className="text-base">
         This event is operated under a licence from TED
       </p>
-      <p className="font-heading">
+      <p className="text-base">
         Created with <span className="text-red-600">❤</span> by{" "}
         <a
           href="https://www.shouryanaik.com"
