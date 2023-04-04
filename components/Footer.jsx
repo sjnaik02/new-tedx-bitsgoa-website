7b7fa5a956d09eb03271484f5cbbda3d7e0d038a
Shourya Naik <sjnaik02@gmail.com>
2023-04-04 19:46:55 +0530

    heavenly crust updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -57,7 +57,7 @@ export default function Footer() {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              class="feather feather-instagram h-6 w-6"
+              className="feather feather-instagram h-6 w-6"
             >
               <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
@@ -79,7 +79,7 @@ export default function Footer() {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              class="feather feather-twitter"
+              className="feather feather-twitter"
             >
               <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
             </svg>
@@ -99,7 +99,7 @@ export default function Footer() {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              class="feather feather-linkedin"
+              className="feather feather-linkedin"
             >
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
               <rect x="2" y="9" width="4" height="12"></rect>
