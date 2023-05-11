b6743c146dd28a98d0420232c04aca411cbd8779
Shourya Naik <sjnaik02@gmail.com>
2023-05-11 13:38:23 +0530

    minor fixes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -54,9 +54,9 @@ export default function Footer() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="feather feather-instagram h-6 w-6"
             >
               <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
